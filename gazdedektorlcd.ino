1aa8da4ef6c43a1976a4f18f1fb6b7da4d749cd7
reyhancaba <47894149+reyhancaba@users.noreply.github.com>
2019-04-13 20:14:47 +0300

    gaz dedektörü
    kodun döngü kısmı eklendi.

--- gazdedektorlcd.ino
+++ gazdedektorlcd.ino
@@ -28,4 +28,35 @@ int deger;                          //Sensörden okunan değer
  Serial.begin(9600);                           // Seri haberleşmeyi başlatmış oldum.
  pinMode(BuzzerPin, OUTPUT);                   //Buzzer pinini çıkış olarak ayarlıyoruz.
  }
+void loop () {
+   GelenDeger = analogRead(A0);                //A0 pininden analog değer okuma
+  GelenDegerGerilim=GelenDeger*(5000/1023);    //0-1023 arası değer gelecektir. 0 = 0V / 1023=5V temsil eder.
+                                               // bu değeri mV cinsinden alıyoruz.  
+  GazMiktari=GelenDegerGerilim/26;             //LCD de daha düşük rakamların gözükmesi için scala ediyorum.
+                                               //sıcaklık değerini hesaplıyoruz
+  lcd.setCursor(0,1);                          //0 sütun, 1 satır numarası. (saymaya 0 dan başladığımız için aslında 2.satırı temsil eder.)
+  
+  lcd.print(GazMiktari);                       //Gaz değişkeninin değerini ekrana yazdıralım.
+
+ 
+  delay(1000);                                 //1 saniye aralıklarla gaz değişimini kontrol edelim.
+  
+ a=analogRead(A0);                  // Gaz sensörümüzün analog çıkış ucunu arduinomuza bağladığımız kısım
+ Serial.println(a);                 // Sonucu ekrana yazdırıyoruz. 
+ deger = analogRead(A0);            //Sensörden analog değer okuyoruz.
+  if(deger > esikDegeri){           //Sensörden okunan değer eşik değerinden büyükse çalışır.
+    lcd.setCursor(0,1);
+    lcd.print("GAZ KACAGI VAR");
+    delay(500);
+    lcd.clear();
+    digitalWrite(BuzzerPin, HIGH);
+    delay(100);
+    digitalWrite(BuzzerPin, LOW);
+    delay(100);
+  }
+  else{                             //Sensörden okunan değer eşik değerinin altındaysa çalışır.
+   lcd.setCursor(0,0);             //LCDde yazılacak satırı seçiyoruz.
+   lcd.print("TEHLIKE YOK");
+   digitalWrite(BuzzerPin, LOW);
+  }}
  

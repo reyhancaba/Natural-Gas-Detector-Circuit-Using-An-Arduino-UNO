4f92f2ee1b21573f788e7441148fbe71f446561c
reyhancaba <47894149+reyhancaba@users.noreply.github.com>
2019-04-16 21:53:38 +0300

    Update gazdedektorlcd.ino

--- gazdedektorlcd.ino
+++ gazdedektorlcd.ino
@@ -15,36 +15,37 @@ int deger;                          //Sensörden okunan değer
 
 
   void setup(){
-     // LCD sütun ve satır sayısı ayarlandı.
-  lcd.begin(16, 2);
-  // LCD ye mesaj yazdırdım. (isteğe göre değişebilir)
-  lcd.print("Algılanıyor..");
-  // Algılanıyor..  yazısından sonra 2 saniye beklettim.
-  delay(2000);
-  //Ekranı temizledim ve daha sonra Gaz değerini yazdirdim.
-  lcd.clear();
-  lcd.print("GAZ MIKTARI:");
+                                   // LCD sütun ve satır sayısı ayarlandı.
+     lcd.begin(16, 2);
+                                   // LCD ye mesaj yazdırdım. (isteğe göre değişebilir)
+     lcd.print("Algilaniyor..");
+                                  // Algilaniyor..  yazısından sonra 2 saniye beklettim.
+     delay(2000);
+                                  //Ekranı temizledim ve daha sonra Gaz değerini yazdirdim.
+     lcd.clear();
+     lcd.print("GAZ MIKTARI:");
   
- Serial.begin(9600);                           // Seri haberleşmeyi başlatmış oldum.
- pinMode(BuzzerPin, OUTPUT);                   //Buzzer pinini çıkış olarak ayarlıyoruz.
+    Serial.begin(9600);                           // Seri haberleşmeyi başlatmış oldum.
+    pinMode(BuzzerPin, OUTPUT);                   //Buzzer pinini çıkış olarak ayarlıyoruz.
  }
 void loop () {
    GelenDeger = analogRead(A0);                //A0 pininden analog değer okuma
-  GelenDegerGerilim=GelenDeger*(5000/1023);    //0-1023 arası değer gelecektir. 0 = 0V / 1023=5V temsil eder.
+   GelenDegerGerilim=GelenDeger*(5000/1023);    //0-1023 arası değer gelecektir. 0 = 0V / 1023=5V temsil eder.
                                                // bu değeri mV cinsinden alıyoruz.  
-  GazMiktari=GelenDegerGerilim/26;             //LCD de daha düşük rakamların gözükmesi için scala ediyorum.
+   GazMiktari=GelenDegerGerilim/26;             //LCD de daha düşük rakamların gözükmesi için scala ediyorum.
                                                //sıcaklık değerini hesaplıyoruz
-  lcd.setCursor(0,1);                          //0 sütun, 1 satır numarası. (saymaya 0 dan başladığımız için aslında 2.satırı temsil eder.)
+   lcd.setCursor(0,1);                          //0 sütun, 1 satır numarası. (saymaya 0 dan başladığımız için aslında 2.satırı temsil eder.)
   
   lcd.print(GazMiktari);                       //Gaz değişkeninin değerini ekrana yazdıralım.
 
  
   delay(1000);                                 //1 saniye aralıklarla gaz değişimini kontrol edelim.
   
- a=analogRead(A0);                  // Gaz sensörümüzün analog çıkış ucunu arduinomuza bağladığımız kısım
- Serial.println(a);                 // Sonucu ekrana yazdırıyoruz. 
- deger = analogRead(A0);            //Sensörden analog değer okuyoruz.
-  if(deger > esikDegeri){           //Sensörden okunan değer eşik değerinden büyükse çalışır.
+ a=analogRead(A0);                             // Gaz sensörümüzün analog çıkış ucunu arduinomuza bağladığımız kısım
+ Serial.println(a);                            // Sonucu ekrana yazdırıyoruz. 
+ deger = analogRead(A0);                       //Sensörden analog değer okuyoruz.
+  if(deger > esikDegeri)
+  {                                            //Sensörden okunan değer eşik değerinden büyükse çalışır.
     lcd.setCursor(0,1);
     lcd.print("GAZ KACAGI VAR");
     delay(500);
